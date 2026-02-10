0ec3636fb40f2786fabb7778dad7d4bda19748d3
zulfa2024 <zulfaoumer1@gmail.com>
2026-02-09 19:25:14 -0800

    added

--- task-manager/lib/mongodb.ts
+++ task-manager/lib/mongodb.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
-
+// Flag to track the connection status
 let isConnected = false;
+// Function to connect to MongoDB
 
 export async function connectDB() {
   if (isConnected) return;
